0e2fee2a8064d4aea14eb47c4424b4553ab7c0b4
Gary Krause <gary@codechief.dev>
2022-05-08 23:12:54 -0400

    dir dir dir

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+/* eslint-disable */
+const path = require("path");
+
 module.exports = {
   publicPath: "/shenandoahbitcoinclub",
+  outputDir: path.resolve(__dirname, "./dist"),
+  assetsDir: "dist",
 };
