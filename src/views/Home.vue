6a7e6bf67b7c48a9d0aaebce493709285f58ae0c
Gary Krause <gary@codechief.dev>
2022-05-11 20:49:18 -0400

    Image handling.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
     <h3>Please RSVP with gary@codechief.dev if you plan on attending.</h3>
     <h1>Shenandoah Bitcoin Club</h1>
     <p>Educating an collaborating on Bitcoing in the Shenandoah Valley.</p>
-    <img alt="Bitcoin symbol" src="../assets/logo.png" />
+    <img alt="Bitcoin symbol" src="../assets/logo.png" class="center-fit" />
   </div>
 </template>
 
@@ -24,3 +24,10 @@ export default defineComponent({
   name: "Home",
 });
 </script>
+<style scoped>
+.center-fit {
+  max-width: 100%;
+  max-height: 100vh;
+  margin: auto;
+}
+</style>
