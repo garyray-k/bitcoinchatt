a6d9c5b6c3d7984a23b16ac363a987aa0aa8700c
Gary Krause <gary@codechief.dev>
2022-06-16 06:25:01 -0400

    AMA advert.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="home">
+    <h2>
+      Impromptu AMA on the Winchester Downtown walk, this Friday evening, June
+      17th, 2022
+    </h2>
+    <p>
+      Look for the guy at the table with the Bitcoin sign and t-shirt. ~5pm-??
+    </p>
+    <br />
     <h2>Next Meeting: July 8th, 2022 at 7pm. Location: TBD</h2>
     <h3>
       Please RSVP with gary@codechief.dev or on
