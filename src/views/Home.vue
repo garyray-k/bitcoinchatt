55b76fd924d9baa82528762356aa7f56528a2ad3
Gary Krause <gary@codechief.dev>
2022-06-18 14:18:13 -0400

    Update meetup date.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="home">
     <h2>
-      Impromptu AMA on the Winchester Downtown walk, this Friday evening, June
-      17th, 2022
+      Next Meeting: July 8th, 2022 at 7pm. Location:
+      <a href="https://delsolcafes.com/locations/winchester">Cafe Del Sol</a>
     </h2>
-    <p>
-      Look for the guy at the table with the Bitcoin sign and t-shirt. ~5pm-??
-    </p>
-    <br />
-    <h2>Next Meeting: July 8th, 2022 at 7pm. Location: TBD</h2>
     <h3>
       Please RSVP with gary@codechief.dev or on
       <a href="https://fb.me/e/4Xl3QUwNy">Facebook</a> if you plan on attending.
